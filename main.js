4abacee7c37e0bf7c854a7c757f08e6f7ce11447
Chris Davis <disscravis@chriss-mbp.lan>
2022-08-24 18:16:09 -0700

    updated for button

---
+++ main.js
@@ -0,0 +1,23 @@
+// we want to scan through the elements on a webpage and search for keywords like "instructions",
+//"directions" or "ingredients"
+//If those keywords are found, we will use const ingredientsWord = document.documentElement.innerText.indexOf('ingredients');
+//ingredientsWord.scrollIntoView({behavior: "smooth"})
+
+
+//how can I "capture" the html text of any website that I visit
+//if the words "instructions" "directions" "ingredients" exist we will auto scroll to those areas
+
+
+
+
+
+//examples below of how to possibly scroll the element into view once we are able to target
+//text that indicates that the recipe is there
+
+
+// const element = document.getElementById("box");
+
+// element.scrollIntoView();
+// element.scrollIntoView(false);
+// element.scrollIntoView({block: "end"});
+// element.scrollIntoView({behavior: "smooth", block: "end", inline: "nearest"});
